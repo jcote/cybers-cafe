04c5f829a4668ec23251d90c9a533ca12f6eec1a
Jordan Cote <jordocote@gmail.com>
2016-12-15 17:09:28 -0500

    Maybe rigidbody teleport

--- canvas/files/assets/5485279/1/Network.js
+++ canvas/files/assets/5485279/1/Network.js
@@ -150,7 +150,9 @@ Network.prototype.addEntity = function(data) {
     entity.setLocalPosition(data.position[0],data.position[1],data.position[2]);
     entity.setLocalScale(data.scale[0],data.scale[1],data.scale[2]);
     entity.setLocalRotation(data.rotation[0],data.rotation[1],data.rotation[2],1);
-    entity.rigidbody.teleport(data.position[0],data.position[1],data.position[2]);
+    if (entity.rigidbody) {
+      entity.rigidbody.teleport(data.position[0],data.position[1],data.position[2]);
+    }
     this.app.root.addChild(entity);
 
     console.log('Entity Added');
