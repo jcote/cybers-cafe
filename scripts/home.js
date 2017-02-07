6281d4c74434a165dcd82dda9da918ded5f91277
Jordan Cote <jordocote@gmail.com>
2017-02-07 09:09:37 -0500

    Add image form on client side

--- scripts/home.js
+++ scripts/home.js
@@ -14,6 +14,15 @@ function drawer () {
   }
 }
 
+var forms = ["createEntityImageForm", "createEntityModelForm"];
+
+function selectForm (form) {
+  for (var i = 0; i < forms.length; i++) {
+  	document.getElementById(forms[i]).style.display = 'none';
+  }
+  document.getElementById(form).style.display = 'block';
+}
+
 $(function(){
 	var form = document.forms.namedItem("createEntityModel");
 	form.addEventListener('submit', function(ev) {
@@ -25,6 +34,31 @@ $(function(){
 
 	  var oReq = new XMLHttpRequest();
 	  oReq.open("POST", "api/entities", true);
+	  oReq.onload = function(oEvent) {
+	    if (oReq.status == 200) {
+	      var responseJson = JSON.parse(oReq.responseText);
+	      oOutput.innerHTML = responseJson.message;
+	    } else {
+	      oOutput.innerHTML = "Error occurred.<br \/>";
+	    }
+	  };
+
+	  oReq.send(oData);
+	  ev.preventDefault();
+	}, false);
+});
+
+$(function(){
+	var form = document.forms.namedItem("createEntityImage");
+	form.addEventListener('submit', function(ev) {
+	  var oOutput = document.getElementById("imageFormResultContainer"),
+	      oData = new FormData(form);
+
+      oOutput.innerHTML = "Uploading...";
+	//  oData.append("CustomField", "This is some extra data");
+
+	  var oReq = new XMLHttpRequest();
+	  oReq.open("POST", "api/image", true);
 	  oReq.onload = function(oEvent) {
 	    if (oReq.status == 200) {
 	      var responseJson = JSON.parse(oReq.responseText);
