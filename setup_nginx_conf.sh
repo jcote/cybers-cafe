edbd53a02cf41d5475b55c30f001f3568b54e99c
jcote <jordocote@gmail.com>
2020-04-02 17:35:40 +0000

    added ssl config for nginx

--- setup_nginx_conf.sh
+++ setup_nginx_conf.sh
@@ -4,3 +4,7 @@ ln -s /etc/nginx/sites-available/www.cybers.cafe-ssl.conf /etc/nginx/sites-enabl
 
 ln -s /var/www/cybers-cafe/cybers-cafe/system/files/cybers.cafe-ssl.conf /etc/nginx/sites-available/cybers.cafe-ssl.conf
 ln -s /etc/nginx/sites-available/cybers.cafe-ssl.conf /etc/nginx/sites-enabled/cybers.cafe-ssl.conf 
+
+openssl dhparam -out /etc/nginx/snippets/dhparam.pem 4096
+
+cp /var/www/cybers-cafe/cybers-cafe/system/snippets/ssl-params.conf /etc/nginx/snippets/ssl-params.conf
