1d527ae11dd73aa66a282109f9b93a7593d25ed3
jcote <jordocote@gmail.com>
2020-04-02 17:13:09 +0000

    setup nginx script

---
+++ setup_nginx_conf.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ln -s /var/www/cybers-cafe/cybers-cafe/system/files/www.cybers.cafe-ssl.conf /etc/nginx/sites-available/www.cybers.cafe-ssl.conf
+ln -s /etc/nginx/sites-available/www.cybers.cafe-ssl.conf /etc/nginx/sites-enabled/www.cybers.cafe-ssl.conf 
+
+ln -s /var/www/cybers-cafe/cybers-cafe/system/files/cybers.cafe-ssl.conf /etc/nginx/sites-available/cybers.cafe-ssl.conf
+ln -s /etc/nginx/sites-available/cybers.cafe-ssl.conf /etc/nginx/sites-enabled/cybers.cafe-ssl.conf 
