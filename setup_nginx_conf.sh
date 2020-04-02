e1a97b2cc281eac761de8bb9f3cb0184feb4bea7
jcote <jordocote@gmail.com>
2020-04-02 18:51:59 +0000

    ssl, nginx config, pm2 script

--- setup_nginx_conf.sh
+++ setup_nginx_conf.sh
@@ -5,6 +5,6 @@ ln -s /etc/nginx/sites-available/www.cybers.cafe-ssl.conf /etc/nginx/sites-enabl
 ln -s /var/www/cybers-cafe/cybers-cafe/system/files/cybers.cafe-ssl.conf /etc/nginx/sites-available/cybers.cafe-ssl.conf
 ln -s /etc/nginx/sites-available/cybers.cafe-ssl.conf /etc/nginx/sites-enabled/cybers.cafe-ssl.conf 
 
-openssl dhparam -out /etc/nginx/snippets/dhparam.pem 4096
+openssl dhparam -out /etc/ssl/certs/dhparam.pem 4096
 
 cp /var/www/cybers-cafe/cybers-cafe/system/snippets/ssl-params.conf /etc/nginx/snippets/ssl-params.conf
